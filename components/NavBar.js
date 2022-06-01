676d58a547adf287c9fd0def03ba2fee9410460b
leedhfsd <leedhfsd@gmail.com>
2022-06-01 14:49:02 +0900

    page update

--- components/NavBar.js
+++ components/NavBar.js
@@ -29,16 +29,16 @@ export default function NavBar() {
           <a className={router.pathname === "/event" ? "active" : "common"}>이벤트</a>
         </Link>
         <Link href="/profit-view">
-          <a className={router.pathname === "/profit" ? "active" : "common"}>수익 조회</a>
+          <a className={router.pathname === "/profit-view" ? "active" : "common"}>수익 조회</a>
         </Link>
         <Link href="/profit-enter">
-          <a className={router.pathname === "/profit" ? "active" : "common"}>수익 입력</a>
+          <a className={router.pathname === "/profit-enter" ? "active" : "common"}>수익 입력</a>
         </Link>
         <Link href="/spending-view">
-          <a className={router.pathname === "/spending" ? "active" : "common"}>지출 조회</a>
+          <a className={router.pathname === "/spending-view" ? "active" : "common"}>지출 조회</a>
         </Link>
         <Link href="/spending-enter">
-          <a className={router.pathname === "/spending" ? "active" : "common"}>지출 입력</a>
+          <a className={router.pathname === "/spending-enter" ? "active" : "common"}>지출 입력</a>
         </Link>
         <style jsx>{`
           .active {
