94b18c55e08da235ddbbbfc6d8e01a1d6e85395c
leedhfsd <leedhfsd@gmail.com>
2022-06-04 16:05:28 +0900

    revise tab

--- pages/profit-enter.js
+++
@@ -1,5 +0,0 @@
-export default function ProfitEnter() {
-  return <div>
-    <h1>수익입력</h1>
-  </div>
-}
---
+++ pages/transaction-enter.js
@@ -0,0 +1,5 @@
+export default function TransactionEnter() {
+  return <div>
+    <h1>수익/지출입력</h1>
+  </div>
+}
